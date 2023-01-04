72f33f941092bfa9657f887bd1e844d97dce6127
Setyawan <setyawan.prayogi@student.unmer.ac.id>
2023-01-04 23:22:52 +0700

    user list

--- parkir/listpetugas.php
+++ parkir/listpetugas.php
@@ -2,7 +2,7 @@
     require "../conn.php";
 
     
-    $data       = mysqli_query($con, "SELECT users.id, users.email, users.password, users.nama, role.nama_role, users.id_role, users.createdDate FROM users INNER JOIN role ON users.id_role=role.id_role");
+    $data       = mysqli_query($con, "SELECT users.id, users.email, users.password, users.nama, role.nama_role, users.id_role, users.createdDate FROM users INNER JOIN role ON users.id_role=role.id_role WHERE id=".$_GET['id']); 
     $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
 
     echo json_encode($data);
